ed6ebdbefdeb1475db3963350b8cc1df38c48edc
Gabogogi <bencitymaina@gmail.com>
2023-12-09 21:35:25 +0300

    task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Install Nginx and set up the file system
 
-# Install nginx if it is not already installed
+# Install nginx if not  installed
 apt-get update
 apt-get -y install nginx
 
@@ -13,14 +13,10 @@ for dir in "${DIRS[@]}"; do
     fi
 done
 
-echo "\
-<html>
-  <head>
-  </head>
-  <body>
-    Hooray! The Nginx server is up
-  </body>
-</html>" >> /data/web_static/releases/test/index.html
+echo "
+Go tell to Sparta, thou who passest by,
+that here, obedient to her laws, we lie."
+>> /data/web_static/releases/test/index.html
 
 link_path="/data/web_static/current"
 target_path="/data/web_static/releases/test"
@@ -34,23 +30,29 @@ chgrp -R ubuntu /data/
 # the URL path /hbnb_static
 
 # Create file to be served in case of an error 404
-echo "Ceci n'est pas une page" > /var/www/html/404.html
+echo "Ceci n'est pas une page" > /etc/nginx/html/404.html
 
 # Create the default index page
-echo "Hello World!" > /var/www/html/index.html
+echo "Hello World!" > /etc/nginx/html;
 
 echo "server {
     listen 80;
     listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root /var/www/html;
+    add_header X-Served-By $hostname;
+    root /etc/nginx/html;
 
-    error_page 404 /404.html;
+    
 
     index index.html index.htm index.nginx-debian.html;
 
     location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        return 301 https://www.youtube.com/watch?v=lBP7QQYN1IU;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /etc/nginx/html;
+      internal;
     }
 
     location /hbnb_static {
