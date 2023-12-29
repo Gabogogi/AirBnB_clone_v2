452437889a06915c59663ba83fec12afb3d4e16a
Gabogogi <bencitymaina@gmail.com>
2023-12-29 05:32:12 -0800

    task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Script that runs an app with Flask framework """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_session(exception):
+    """ Teardown """
+    storage.close()
+
+
+@appp.route('/states_list', strict_slashes=False)
+def display_states():
+    '''List all states '''
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
