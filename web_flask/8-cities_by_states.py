a2bc21e28cf6b758bc4cfe29ac95f968e28a4b7a
Gabogogi <bencitymaina@gmail.com>
2023-12-30 15:05:10 +0300

    task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
+from models.city import City
 
 app = Flask(__name__)
 
@@ -14,13 +14,13 @@ def teardown_session(exception):
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def display_states():
-    '''List all states '''
-    states = list(storage.all(State).values())
-    # Sort states by name alphabetically in ascending order
-    states = sorted(states, key=lambda state: state.name)
-    return render_template("7-states_list.html", states=states)
+@app.route('/cities_by_states', strict_slashes=False)
+def display_html():
+    """ Function called with /states_list route """
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html',
+                           Table="States",
+                           states=states)
 
 
 if __name__ == "__main__":
