76868eb61a1de73e4d6fb5efa7890ab8c52c0dd7
Gabogogi <bencitymaina@gmail.com>
2023-12-30 14:49:08 +0300

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ Script that runs an app with Flask framework """
 from flask import Flask, render_template
-#from models import storage
-#from models.state import State
+from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
